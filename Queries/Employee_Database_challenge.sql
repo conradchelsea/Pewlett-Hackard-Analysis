2698dd0571bb082cd4311adc8291eebf8ca22c91
conradchelsea <71476009+conradchelsea@users.noreply.github.com>
2020-11-01 13:20:58 -0600

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,48 +1,5 @@
 
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
-
-INTO nameyourtable
-FROM _______
-ORDER BY _____, _____ DESC;
-
-1
-select emp_no, first_name, last_name from employees
-2
-select  title, from_date, to_date from titles
-3 & 4
--- Joining departments and dept_manager tables
--- SELECT 
---           employees.emp_no,
--- 		            employees.first_name,
--- 					employees.last_name,
--- 			titles.title,		
---      titles.from_date,
---      titles.to_date
--- -- 	 INTO current_employees
--- FROM employees
--- INNER JOIN titles
--- ON employees.emp_no = titles.emp_no;
-
-SELECT 
-          employees.emp_no,
-		            employees.first_name,
-					employees.last_name,
-			titles.title,		
-     titles.from_date,
-     titles.to_date
--- 	 INTO current_employees
-FROM employees
-INNER JOIN titles
-ON employees.emp_no = titles.emp_no
-WHERE employees.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
-Order By employees.emp_no, titles.from_date desc;
-
-DROP TABLE current_employees
-
+-- Create Retirement_Titles Table based on BirthDate
 SELECT 
           employees.emp_no,
 		            employees.first_name,
@@ -57,15 +14,6 @@ ON employees.emp_no = titles.emp_no
 WHERE employees.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 Order By employees.emp_no, titles.from_date desc;
 
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
-
-INTO nameyourtable
-FROM _______
-ORDER BY _____, _____ DESC;
 
 -- Select Unique_Titles from Retirement_Titles
 SELECT DISTINCT ON (emp_no)
