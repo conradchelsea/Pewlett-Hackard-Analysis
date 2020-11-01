7604ce38a89130598271d0ddefc3503824ce995b
conradchelsea <71476009+conradchelsea@users.noreply.github.com>
2020-11-01 13:35:24 -0600

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -34,3 +34,22 @@ FROM unique_titles
 GROUP BY title
 ORDER BY title_count DESC;
 
+
+-- Create Mentorship Table based on BirthDate, Dept to date(current employees only)
+SELECT DISTINCT ON (employees.emp_no)
+          employees.emp_no,
+		            employees.first_name,
+					employees.last_name,
+					employees.birth_date,		
+     dept_emp.from_date,
+     dept_emp.to_date,
+	 titles.title
+INTO Mentorship_eligibility
+FROM employees
+INNER JOIN titles
+ON employees.emp_no = titles.emp_no
+INNER JOIN dept_emp
+ON dept_emp.emp_no = employees.emp_no
+WHERE dept_emp.to_date = '9999-01-01' and employees.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+Order By employees.emp_no, dept_emp.from_date desc;
+
